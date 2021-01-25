501a762d47f2a7768107a9fad65a27fc8ac4d635
SadheeraMahanama <sadheeramahanama94@gmail.com>
2021-01-25 20:54:22 +0530

    reset password fixed

--- src/modules/auth/SignUp.jsx
+++ src/modules/auth/SignUp.jsx
@@ -37,7 +37,7 @@ class SignUp extends Component {
                                     className="auth-form-control"
                                     name="name"
                                     component={InputField}
-                                    placeholder="name"
+                                    placeholder="Name"
                                     aria-describedby="helpId"
                                     autoComplete="on"
 
@@ -50,7 +50,7 @@ class SignUp extends Component {
                                     className="auth-form-control"
                                     name="email"
                                     component={InputField}
-                                    placeholder="email"
+                                    placeholder="Email"
                                     aria-describedby="helpId"
                                     autoComplete="on"
 
@@ -88,7 +88,7 @@ class SignUp extends Component {
                             <button type="button" className="btn auth-button" type="submit" disabled={signUp.pending}>{signUp.pending ?
                                 <div className="spinner-border" role="status">
                                     <span className="sr-only"></span>
-                                </div> : "Signup"}</button>
+                                </div> : "SIGN UP"}</button>
 
                             <hr />
                             <Link className="auth-fp" to="/sign-in">Sign In</Link>
@@ -105,20 +105,20 @@ class SignUp extends Component {
 const validate = values => {
     const errors = {};
     if (!values.name) {
-        errors.name = "Name is Required";
+        errors.name = "Name is required";
     }
     if (!values.email) {
-        errors.email = "Email is Required";
+        errors.email = "Email is required";
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = "Invalid email";
     }
     if (!values.password) {
-        errors.password = "Password is Required";
+        errors.password = "Password is required";
     } else if (values.password.length < 6) {
         errors.password = "Password must be at least 6 characters";
     }
     if (!values.confirmPassword) {
-        errors.confirmPassword = "Confirm Password is Required";
+        errors.confirmPassword = "Confirm Password is required";
     }
     if (values.password && values.confirmPassword && (values.confirmPassword !== values.password)) {
         errors.confirmPassword = "Password mismatch";
