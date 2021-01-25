70f483b6fd5fc14edcbd93cbd18274a7168f6400
SadheeraMahanama <sadheeramahanama94@gmail.com>
2021-01-25 12:07:02 +0530

    redirect route fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import history from "./_helpers/history";
 import routes from "./routes";
 
@@ -16,6 +16,7 @@ const App = () => (
           />
         ))}
       </Switch>
+      <Redirect from="/" to="/login" />
     </Router>
   </div>
 );
